699c924d7c22c20568b7f7a8e0ca67fee09e4bba
Zetnavrek <vykovi@gmail.com>
2018-05-21 11:13:00 -0500

    Repaso para aprender a usar git hub

---
+++ js/app.js
@@ -0,0 +1,30 @@
+var peliculas = [
+    {
+        titulo:'La mala noticia',
+        director:'fulanito1',
+        protagonistas:['fulanita1','fulanita1','fulanita2'],
+        genero:'romance',
+        'horario funciones':['12:00','13:00','14:00']
+    },
+    {
+        titulo:'La buena noticia',
+        director:'fulanito2',
+        protagonistas:['fulanita1','fulanita1','fulanita2'],
+        genero:'terror',
+        'horario funciones':['12:00','13:00','14:00']
+    },
+    {
+        titulo:'Todo lo que hay en medio',
+        director:'fulanito3',
+        protagonistas:['fulanita1','fulanita1','fulanita2'],
+        genero:'ficcion',
+        'horario funciones':['12:00','13:00','14:00']
+    },
+    {
+        titulo:'Una cancion de rap muy mala',
+        director:'fulanito4',
+        protagonistas:['fulanita1','fulanita1','fulanita2'],
+        genero:'romance',
+        'horario funciones':['12:00','13:00','14:00']
+    }
+];
